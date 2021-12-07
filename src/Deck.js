8ad1cb9ddb7719931aaf5aedad1063d88f5f91e2
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-07 10:14:12 -0600

    load a new deck from API

--- src/Deck.js
+++ src/Deck.js
@@ -3,8 +3,21 @@ import Card from './Card';
 import axios from "axios";
 import './Deck.css';
 
-function Deck() {
+const API_BASE_URL = "http://deckofcardsapi.com/api/deck";
 
+function Deck() {
+    const [deck, setDeck] = useState(null);
+    const [drawn, setDrawn] = useState([]);
+    const [autoDraw, setAutoDraw] = useState(false);
+    const timerRef = useRef(null);
+
+    useEffect(() => {
+        async function getData() {
+            let newDeck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
+            setDeck(newDeck.data);
+        }
+        getData();
+    }, [setDeck]);
 }
 
 export default Deck;
